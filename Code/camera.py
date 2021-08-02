ff5149ffa9c853e1826a25888ba77c511e04a39c
Ali <68898777+ALICHOUCHENE@users.noreply.github.com>
2021-08-02 23:47:11 +0100

    Add files via upload

---
+++ Code/camera.py
@@ -0,0 +1,12 @@
+from picamera import PiCamera
+
+camera = PiCamera()
+camera.resolution = (1024, 768)
+
+def take_image():
+	camera.start_preview()
+	# Camera warm-up time
+	sleep(1)
+	camera.capture('foo.jpg')
+	camera.stop_preview()
+	
