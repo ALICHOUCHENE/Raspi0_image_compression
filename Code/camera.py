d347f3919298b18ef28fd78429e8a2a165523d13
Ali <68898777+ALICHOUCHENE@users.noreply.github.com>
2021-08-11 22:53:50 +0100

    Update camera.py

--- Code/camera.py
+++ Code/camera.py
@@ -3,10 +3,10 @@ from time import sleep
 camera = PiCamera()
 camera.resolution = (1024, 768)
 
-def take_image():
+def take_image(name):
 	camera.start_preview()
 	# Camera warm-up time
 	sleep(1)
-	camera.capture('foo.jpg')
+	camera.capture(str(name)+'.jpg')
 	camera.stop_preview()
 	
