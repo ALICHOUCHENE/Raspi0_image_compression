b12eb3067c9ea4f0d56274903c17124f4687d696
Ali <68898777+ALICHOUCHENE@users.noreply.github.com>
2021-08-11 23:07:13 +0100

    Add files via upload

---
+++ Code/main.py
@@ -0,0 +1,19 @@
+import UART
+import camera
+import compress
+
+arduino=UART.connect()
+pyboard=UART.connect()
+list arduino_data=[]
+while True :
+	if arduino.available():
+		data=UART.raed(arduino)
+		arduino_data.append(data)
+		camera.take_image('image_1')
+		compress.compress_image()
+		UART.send_image_1(pyboard,"compressed_image_1")
+		time.sleep(2)
+		camera.take_image('image_2')
+		compress.compress_image()
+		UART.send_image_1(pyboard,"compressed_image_2")
+		time.sleep(2)
