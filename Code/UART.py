6c3422f37dbf523aacbd53ea36b77a13167e2943
Ali <68898777+ALICHOUCHENE@users.noreply.github.com>
2021-08-13 23:24:16 +0100

    Update UART.py

--- Code/UART.py
+++ Code/UART.py
@@ -10,37 +10,32 @@ def ports_availables() :
 
 def connect(com):
 
-    baudrate=9600
-    bytesize=8
-    try:
-    port = serial.Serial(com,baudrate,bytesize,timeout=1)
-   		 print('connected sucsessefully')
-    except:
-         print('faile')
+	baudrate=9600
+	bytesize=8
+	try:
+		port = serial.Serial(com,baudrate,bytesize,timeout=1)
+	except:
+		print('faile')
 
-    return port 
-
-
+	return port 
 
 def read(port):
-	if port.in_waiting > 0:
-            line = port.readline().decode('utf-8').rstrip()
-    return(line)
+	if (port.in_waiting) > 0 :
+		line = (port.readline().decode('utf-8')).rstrip()
+	return(line)
 
 
-def send_image_1(port,image):            	    # image.jpg
-	
-	os.system('base64 < image > pic.txt')	    #Linux builtin to convert binary files to base64 txt
-	CHUNK_SIZE = 55 					        # Maximum chunk size that can be sent
-    image_file = 'pic.txt'					    # Image converted to b64 txt file
-    with open(image_file, 'rb') as infile:			
-    	while True:
-		
-			chunk = infile.read(CHUNK_SIZE)		
+def send_image_1(port,image):
+	os.system('base64 < image > pic.txt')
+	CHUNK_SIZE = 55
+	image_file ='pic.txt'
+	with open(image_file, 'rb') as infile:	
+		while True:
+			chunk = infile.read(CHUNK_SIZE)
 			if not chunk: break		
-			port.write(chunk) 			    	# write data to Arduin0		
-			port.write('\r')					# Write carriage return to tell arduino to send message
-    port.close()
+			port.write(chunk)
+			port.write('\r')
+		port.close()
 		
 
  
